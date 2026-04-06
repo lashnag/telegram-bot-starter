d541cfc67f66a0d31ec896caaa65393a8749c936
Alexey Lashnev <lashnag@gmail.com>
2026-04-06 12:09:06 +0300

    Add connection timeouts in client

--- src/main/kotlin/com/github/lashnag/telegrambotstarter/TelegramBotAutoConfiguration.kt
+++ src/main/kotlin/com/github/lashnag/telegrambotstarter/TelegramBotAutoConfiguration.kt
@@ -1,9 +1,11 @@
 package com.github.lashnag.telegrambotstarter
 
 import com.pengrad.telegrambot.TelegramBot
+import okhttp3.OkHttpClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class TelegramBotAutoConfiguration {
@@ -11,8 +13,23 @@ class TelegramBotAutoConfiguration {
     @Value("\${telegram-bot-starter.bot.token}")
     private lateinit var botToken: String
 
+    @Value("\${telegram-bot-starter.client.connection-timeout}")
+    private lateinit var connectionTimeout: String
+
+    @Value("\${telegram-bot-starter.client.read-timeout}")
+    private lateinit var readTimeout: String
+
     @Bean
     fun telegramBot(): TelegramBot {
-        return TelegramBot(botToken)
+        val okHttpClient = OkHttpClient.Builder()
+            .callTimeout(connectionTimeout.toLong(), TimeUnit.MILLISECONDS)
+            .connectTimeout(connectionTimeout.toLong(), TimeUnit.MILLISECONDS)
+            .readTimeout(readTimeout.toLong(), TimeUnit.MILLISECONDS)
+            .writeTimeout(readTimeout.toLong(), TimeUnit.MILLISECONDS)
+            .build()
+
+        return TelegramBot.Builder(botToken)
+            .okHttpClient(okHttpClient)
+            .build()
     }
 }
